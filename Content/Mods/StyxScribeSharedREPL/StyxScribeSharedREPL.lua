f8238a6238a6b83865df859ec638288271538e62
Naix <91845220+NaixGames@users.noreply.github.com>
2024-03-30 19:18:12 +0000

    Delete Content/Mods/StyxScribeSharedREPL directory

--- Content/Mods/StyxScribeSharedREPL/StyxScribeSharedREPL.lua
+++
@@ -1,9 +0,0 @@
-ModUtil.Mod.Register( "StyxScribeSharedREPL" )
-
-function StyxScribeSharedREPL.Define( )
-	StyxScribeShared.Root.RunLua = StyxScribeShared.Async( StyxScribeREPL.RunLua )
-end
-
-StyxScribe.AddHook( StyxScribeSharedREPL.Define, "StyxScribeShared: Reset", StyxScribeSharedREPL )
-
-StyxScribeSharedREPL.Define( )
